a5761b0af23c618ea3539bf053cd9bd452da6b6d
Nicku Perdana <nicku.r.perdana@gmail.com>
2023-12-13 16:13:39 +0700

    Add D3 file excercises

---
+++ D3/app.py
@@ -0,0 +1,6 @@
+import pkg.module1 as mod1
+
+fahrenheit = mod1.celsius_to_fahr(60)
+kelvin = mod1.fahr_to_kelvin(fahrenheit)
+
+print(kelvin)
---
+++ D3/pkg/module1.py
@@ -0,0 +1,5 @@
+def celsius_to_fahr(temp):
+    return ((9/5) * temp ) + 32
+
+def fahr_to_kelvin(temp):
+    return ((temp - 32) * 5/9) + 273.15
